af7880edd982e52288b5ca7a3eb6f833dfb18bb7
Julien Adam <julien.adam@arcodia.fr>
2022-12-11 14:10:11 +0100

    Secrets demo

---
+++ ConfigLogsDemos/Controllers/SecretDemoController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConfigLogsDemos.Controllers
+{
+    public class SecretDemoController : Controller
+    {
+        private readonly IConfiguration configuration;
+
+        public SecretDemoController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        public IActionResult Index()
+        {
+            return Content(configuration["configuration:connectionString"]);
+        }
+    }
+}
